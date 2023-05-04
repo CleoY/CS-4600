4e06ca8c2f757d95b09efcfff66d120bf2474b48
CleoY <77142560+CleoY@users.noreply.github.com>
2023-05-03 21:00:26 -0700

    Outline for final project.

---
+++ Final/receiver.cpp
@@ -0,0 +1,4 @@
+// g++ -o myprogram mycode.cpp -lssl -lcrypto
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
---
+++ Final/sender.cpp
@@ -0,0 +1,15 @@
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+/**
+ * Folder Organization:
+ * Final: "Open channel" with Sender and Receiver directories + each party's public key
+ * Sender dir: sender.cpp, files with public and private keys, plaintext file
+ * Receiver dir: receiver.cpp + files with public and private keys
+*/
+
+/**
+ * Outline: 
+ * Method for generating RSA private-public key pair
+ * 
+*/
