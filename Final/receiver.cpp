fe30c56ceff176ffdd59002de0b133684be470d8
CleoY <77142560+CleoY@users.noreply.github.com>
2023-05-03 22:43:43 -0700

    Added to outlines of each class.

--- Final/receiver.cpp
+++ Final/receiver.cpp
@@ -2,3 +2,24 @@
 
 #include <openssl/ssl.h>
 #include <openssl/err.h>
+
+/**
+ * Folder Organization:
+ * Final: "Open channel" with Sender and Receiver directories + each party's public key
+ * Sender dir: sender.cpp, files with public and private keys, plaintext file
+ * Receiver dir: receiver.cpp + files with public and private keys
+ * 
+ * OR Transmitted_Data file includes public keys, encrypted msg + encrypted AES key + MAC
+ *      Sender writes to file while receiver reads from it
+*/
+
+/**
+ * Outline: 
+ * Method for generating RSA private-public key pair
+ * Receive msg from sender
+ * Authenticate MAC by calculating and comparing (?)
+ * Decrypt attached AES key with OWN private key
+ * Decrypt message with decrypted AES key
+ * Read msg
+ * 
+*/
--- Final/sender.cpp
+++ Final/sender.cpp
@@ -6,10 +6,18 @@
  * Final: "Open channel" with Sender and Receiver directories + each party's public key
  * Sender dir: sender.cpp, files with public and private keys, plaintext file
  * Receiver dir: receiver.cpp + files with public and private keys
+ * 
+ * OR Transmitted_Data file includes public keys, encrypted msg + encrypted AES key + MAC
+ *      Sender writes to file while receiver reads from it
 */
 
 /**
  * Outline: 
  * Method for generating RSA private-public key pair
+ * Generate AES key for message
+ * Encrypt message with AES key
+ * Encrypt AES KEY with RECEIVER'S RSA PUBLIC key
+ * Authenticate msg with MAC and append MAC to msg
+ * Send encrypted messaged with encrypted AES key and MAC
  * 
 */
