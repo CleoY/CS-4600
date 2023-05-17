898f8c09f078a58de9ce7733368ff4993e70404c
CleoY <77142560+CleoY@users.noreply.github.com>
2023-05-17 01:29:12 -0700

    Working on parsing package.

--- Final/Receiver/receiver.cpp
+++ Final/Receiver/receiver.cpp
@@ -34,9 +34,14 @@
 */
 
 int getFileSize(const char* fileName);
-int getTail(const char* fileName, const char* parsedPortion, const char* delimiter);
+int splitFile(const char* fileName, const char* delimiter, const char* output1, const char* output2);
 
 int main(){
+    // check what happens if the delimiter cannot be found
+    // Split the full package sent by sender into encrypted message and key and HMAC
+    splitFile("full_package.bin", "\n`````\n", "./Receiver/encrypted_msg_and_key.bin", "./Receiver/parsed_HMAC.bin");
+    
+
 
     return 0;
 }
@@ -71,12 +76,72 @@ int splitFile(const char* fileName, const char* delimiter, const char* output1, 
     // Create output streams for resulting files
     std::ofstream outputFile1(output1, std::ios::binary);
     std::ofstream outputFile2(output2, std::ios::binary);
-    if()
+    if(!outputFile1 || !outputFile2){
+        printf("Error: Could not create output files.\n");
+        return -1;
+    }
+
+    // Read input file
+    std::string fileContent((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
+    
+    // Find delimiter
+    size_t delimiterPosition = fileContent.find(delimiter);
+    if(delimiterPosition != std::string::npos){
+        // Split file in half based on delimiter
+        std::string firstHalf = fileContent.substr(0, delimiterPosition);
+        std::string secondHalf = fileContent.substr(delimiterPosition + strlen(delimiter));
+
+        // Write each half to the output files
+        outputFile1 << firstHalf;
+        outputFile2 << secondHalf;
+    }
+
+    // Close streams
+    inputFile.close();
+    outputFile1.close();
+    outputFile2.close();
 
     return 0;
 }
 
 
-int authenticateHMAC(const char* givenFile){
+int authenticateHMAC(const char* keyFile, const char* inputFile){
+    // Read HMAC key from file given in keyFile parameter
+    unsigned char HMAC_key[EVP_MAX_KEY_LENGTH];
+    memset(HMAC_key, 0, EVP_MAX_KEY_LENGTH);
+    FILE *key_fp = fopen(keyFile, "rb");
+    if(key_fp == nullptr){
+        printf("The HMAC key does not exist. Please generate a key and share it between both parties.\n");
+        fclose(key_fp);
+        return -1;
+    }
+    fread(HMAC_key, 1, EVP_MAX_KEY_LENGTH, key_fp);
+    fclose(key_fp);
 
+    // Read data from inputFile
+    FILE* input_fp = fopen(inputFile, "rb");
+    if(input_fp == NULL){
+        printf("Could not find input file.\n");
+        return -1;
+    }
+    int fileSize = getFileSize(inputFile);
+    std::vector<unsigned char> dataBuffer(fileSize);
+    size_t readBytes = fread(dataBuffer.data(), 1, fileSize, input_fp);
+    fclose(input_fp);
+    if(readBytes != fileSize){ // if readBytes < fileSize, reading did not finish
+        printf("Error in reading the input file data.\n"); 
+        return -1;
+    }
+    
+    // Calculate HMAC-SHA256
+    unsigned char calc_hmac[EVP_MAX_MD_SIZE];
+    unsigned int hmacLength; ////// not init = ok?
+    memset(calc_hmac, 0, EVP_MAX_MD_SIZE);
+    HMAC(EVP_sha256(), HMAC_key, EVP_MAX_KEY_LENGTH, dataBuffer.data(), dataBuffer.size(), calc_hmac, &hmacLength);
+
+    // Verify given HMAC with calculated HMAC
+
+
+
+    return 0;
 }
